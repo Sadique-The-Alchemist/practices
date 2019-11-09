661d2565817ba9efb61ed7322dc1c104e40ff40d
mohammedsadiquepalakkal <sadique.pal@gmail.com>
2019-11-09 12:44:57 +0530

    refactor- using mix function instead of darken and lighten

--- colors/lib/colors.ex
+++ colors/lib/colors.ex
@@ -4,7 +4,7 @@ defmodule Colors do
   """
 
   import CssColors
-  @ranges [0.01, 0.04, 0.09, 0.16, 0.27, 0.36, 0.49, 0.64, 0.81, 1]
+  @ranges [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
   @doc """
   it expects a string of hexcode of a color and it delegates to_black and to_white functions map of black and white
   variations
@@ -15,21 +15,23 @@ defmodule Colors do
   end
 
   @doc """
-  it return a map of 10 darkened variations of given color by CssColors.darken/2 function in a stream and used parse!/1 for string
+  it return a map of 10 darkened variations of given color by CssColors.mix/3 function in a stream and used parse!/1 for string
    to rgb and used rgb/1 for hsl to rgb and to_strign/1 for rgb to hex code and wrapped each element to atuple alongside with index 
    and converted to a map by Enum.reduce and 
   """
   def to_dark(color) do
     pcolor = parse!(color)
+    dcolor = parse!("#000000")
 
-    Stream.map(@ranges, &darken(pcolor, &1))
+    Stream.map(@ranges, &mix(pcolor, dcolor, &1))
     |> color_strem_map
   end
 
   def to_light(color) do
     pcolor = parse!(color)
+    dcolor = parse!("#ffffff")
 
-    Stream.map(@ranges, &lighten(pcolor, &1))
+    Stream.map(@ranges, &mix(pcolor, dcolor, &1))
     |> color_strem_map
   end
 
